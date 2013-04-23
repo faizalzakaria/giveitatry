bef0e2304e275457c5711e6d1714e6d24f21e55a
Faizal Zakaria <Faizal_Zakaria@sigmadesigns.com>
2013-04-23 12:00:40 -0700

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Giveitatry::Application.config.secret_token = 'fcfe01866c0ecf4b7b267bf0a7853fcac275330e6a616523d62d7590abcff4d7c71fd3ffaf5217d01426e4d5a78ce20297a203cb8e73a07adc04187826264616'
