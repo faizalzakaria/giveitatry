2d1c16cddeb8d845e0ee6468d1078673c00acce6
Faizal Zakaria <Faizal_Zakaria@sigmadesigns.com>
2013-04-25 11:45:14 -0700

    Add validator for url and description

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,10 @@
+
 class Image < ActiveRecord::Base
   attr_accessible :description, :url
+
+  validates :url, :presence => true, :uniqueness => true, :uri => {
+    :format => /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix
+  }
+  validates :description, :presence => true, :length => { :minimum => 5 }
+
 end
