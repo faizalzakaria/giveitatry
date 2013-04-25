5b006292e71f98fe65647c6a0f14cd8c13fbfcc2
Faizal Zakaria <Faizal_Zakaria@sigmadesigns.com>
2013-04-25 15:15:59 -0700

    Add validator to check if its image url

--- lib/uri_validator.rb
+++ lib/uri_validator.rb
@@ -1,9 +1,5 @@
 require 'net/http'
 
-# Thanks Ilya! http://www.igvita.com/2006/09/07/validating-url-in-ruby-on-rails/
-# Original credits: http://blog.inquirylabs.com/2006/04/13/simple-uri-validation/
-# HTTP Codes: http://www.ruby-doc.org/stdlib/libdoc/net/http/rdoc/classes/Net/HTTPResponse.html
-
 class UriValidator < ActiveModel::EachValidator
   def validate_each(object, attribute, value)
     raise(ArgumentError, "A regular expression must be supplied as the :format option of the options hash") unless options[:format].nil? or options[:format].is_a?(Regexp)
@@ -13,8 +9,14 @@ class UriValidator < ActiveModel::EachValidator
     if value =~ configuration[:format]
       begin # check header response
         case Net::HTTP.get_response(URI.parse(value))
-          when Net::HTTPSuccess then true
-          else object.errors.add(attribute, configuration[:message]) and false
+        when Net::HTTPSuccess then
+          if remote_file_is_image?(value) then
+            true
+          else
+            object.errors.add(attribute, configuration[:message]) and false
+          end
+        else
+          object.errors.add(attribute, configuration[:message]) and false
         end
       rescue # Recover on DNS failures..
         object.errors.add(attribute, configuration[:message]) and false
@@ -23,4 +25,12 @@ class UriValidator < ActiveModel::EachValidator
       object.errors.add(attribute, configuration[:message]) and false
     end
   end
+
+  def remote_file_is_image?(url)
+    url = URI.parse(url)
+    Net::HTTP.start(url.host, url.port) do |http|
+      return http.head(url.request_uri)['Content-Type'].start_with? 'image'
+    end
+  end
+
 end
